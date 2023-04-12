a128eef1b354f03c3e44a34201a39a22200df443
Jamesson Junior <jamesson.junior@capco.com>
2023-04-12 15:53:02 -0300

    correcao

--- projeto4/main.py
+++ projeto4/main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "<YOUR KEY>"
 
 audio_file = open("C:\work\pessoal\chatgpt\projetos-python\python-chatgpt\projeto4\/audio\german1.mp3", "rb")
 result = openai.Audio.transcribe("whisper-1", audio_file)
